98212fca67cc5cc1a584a58b5d8aa6d104b51ae7
Ilya Erokhin <illyaerohin@yandex.ru>
2021-06-26 20:00:27 +0300

    junit4

---
+++ src/main/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,60 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldIfAmountLess() {
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIfAmountEqualBoundary() {
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIfAmountOver() {
+        int amount = 1100;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIfAmountLessBoundary() {
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIfAmountOverBoundary() {
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+    }
+}
